003227048f9a396dc175b62822de72cc18068950
trank <trank@carleton.edu>
2016-03-10 21:43:42 -0600

    spodermun added all the lists of instruments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,6 +38,10 @@ QStringList g_pipe_list;
 QStringList g_synth_lead_list;
 QStringList g_synth_pad_list;
 QStringList g_synth_effect_list;
+QStringList g_percussive_list;
+QStringList g_ethnic_list;
+QStringList g_percussive_effect_list;
+QStringList g_effects_list;
 
 void write_track_1() {  // P I A N O
   uint16_t start_note = 48;
@@ -307,14 +311,115 @@ void MainWindow::setup_instru_name_list() {
                  <<"30 Overdriven Guitar"
                  <<"31 Distortion Guitar"
                  <<"32 Guitar Harmonics";
-  g_bass_list;
-  g_strings_list;
-  g_ensemble_list;
-  g_brass_list;
-  g_reed_list;
-  g_pipe_list;
-  g_synth_lead_list;
-  g_synth_pad_list;
-  g_synth_effect_list;
+  g_bass_list<<"<None>"
+               <<"33 Acoustic Bass"
+               <<"34 Electric Bass (finger)"
+               <<"35 Electric Bass (pick)"
+               <<"36 Fretless Bass"
+               <<"37 Slap Bass 1"
+               <<"38 Slap Bass 2"
+               <<"39 Synth Bass 1"
+               <<"40 Synth Bass 2";
+  g_strings_list<<"<None>"
+                  <<"41 Violin"
+                  <<"42 Viola"
+                  <<"43 Cello"
+                  <<"44 Contrabass"
+                  <<"45 Tremolo Strings"
+                  <<"46 Pizzicato Strings"
+                  <<"47 Orchestral Harp"
+                  <<"48 Timpani";
+  g_ensemble_list<<"<None>"
+                   <<"49 String Ensemble 1"
+                   <<"50 String Ensemble 2"
+                   <<"51 Synth Strings 1"
+                   <<"52 Synth Strings 2"
+                   <<"53 Choir Aahs"
+                   <<"54 Voice Oohs"
+                   <<"55 Synth Choir"
+                   <<"56 Orchestra Hit";
+
+  g_brass_list<<"<None>"
+                <<"57 Trumpet"
+                <<"58 Trombone"
+                <<"59 Tuba"
+                <<"60 Muted Trumpet"
+                <<"61 French Horn"
+                <<"62 Brass Section"
+                <<"63 Synth Brass 1"
+                <<"64 Synth Brass 2";
+  g_reed_list<<"<None>"
+               <<"65 Soprano Sax"
+               <<"66 Alto Sax"
+               <<"67 Tenor Sax"
+               <<"68 Baritone Sax"
+               <<"69 Oboe"
+               <<"70 English Horn"
+               <<"71 Bassoon"
+               <<"72 Clarinet";
+  g_pipe_list<<"<None>"
+               <<"73 Piccolo"
+               <<"74 Flute"
+               <<"75 Recorder"
+               <<"76 Pan Flute"
+               <<"77 Blown bottle"
+               <<"78 Shakuhachi"
+               <<"79 Whistle"
+               <<"80 Ocarina";
+  g_synth_lead_list<<"<None>"
+                     <<"81 Lead 1 (square)"
+                     <<"82 Lead 2 (sawtooth)"
+                     <<"83 Lead 3 (calliope)"
+                     <<"84 Lead 4 chiff"
+                     <<"85 Lead 5 (charang)"
+                     <<"86 Lead 6 (voice)"
+                     <<"87 Lead 7 (fifths)"
+                     <<"88 Lead 8 (bass + lead)";
+  g_synth_pad_list<<"<None>"
+                    <<"89 Pad 1 (new age)"
+                    <<"90 Pad 2 (warm)"
+                    <<"91 Pad 3 (polysynth)"
+                    <<"92 Pad 4 (choir)"
+                    <<"93 Pad 5 (bowed)"
+                    <<"94 Pad 6 (metallic)"
+                    <<"95 Pad 7 (halo)"
+                    <<"96 Pad 8 (sweep)";
+  g_synth_effect_list<<"<None>"
+                       <<"97 FX 1 (rain)"
+                       <<"98 FX 2 (soundtrack)"
+                       <<"99 FX 3 (crystal)"
+                       <<"100 FX 4 (atmosphere)"
+                       <<"101 FX 5 (brightness)"
+                       <<"102 FX 6 (goblins)"
+                       <<"103 FX 7 (echoes)"
+                       <<"104 FX 8 (sci-fi)";
+    g_ethnic_list<<"<None>"
+                         <<"105 Sitar"
+                         <<"106 Banjo"
+                         <<"107 Shamisen"
+                         <<"108 Koto"
+                         <<"109 Kalimba"
+                         <<"110 Bagpipe"
+                         <<"111 Fiddle"
+                         <<"112 Shanai";
+    g_percussive_list<<"<None>"
+                           <<"113 Tinkle Bell"
+                           <<"114 Agogo"
+                           <<"115 Steel Drums"
+                           <<"116 Woodblock"
+                           <<"117 Taiko Drum"
+                           <<"118 Melodic Tom"
+                           <<"119 Synth Drum"
+                           <<"120 Reverse Cymbal";
+    g_effects_list<<"<None>"
+                    <<"121 Guitar Fret Noise"
+                    <<"122 Breath Noise"
+                    <<"123 Seashore"
+                    <<"124 Bird Tweet"
+                    <<"125 Telephone Ring"
+                    <<"126 Helicopter"
+                    <<"127 Applause"
+                    <<"128 Gunshot";
+
     ui->comboBox_1_instru->addItems(g_piano_list);
 }
