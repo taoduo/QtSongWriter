9affee2922700fbdaf46f07ef4a7b6f1c594f3ca
initial commit <taod@cmc304-09.mathcs.carleton.edu>
2016-03-10 23:28:23 -0600

    finish up

--- mainwindow.h
+++ mainwindow.h
@@ -61,6 +61,8 @@ private slots:
 
     void on_comboBox_5_instru_currentIndexChanged(const QString &arg1);
 
+    void on_comboBox_input_track_currentIndexChanged(int index);
+
 private:
     Ui::MainWindow *ui;
     void init_all_chan_combo_box();
